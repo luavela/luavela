f810986c2749ba2b00f80a03dc7303ff0277e904
Igor Munkin <imun@cpan.org>
2024-02-25 20:40:14 +0300

    Upgrade to Ubuntu 22.04 in GitHub Actions

--- cmake/toolchain/Clang.cmake
+++ cmake/toolchain/Clang.cmake
@@ -5,4 +5,4 @@
 # Copyright (C) 2020-2023 LuaVela Authors. See Copyright Notice in COPYRIGHT
 # Copyright (C) 2015-2020 IPONWEB Ltd. See Copyright Notice in COPYRIGHT
 
-set(CMAKE_C_COMPILER "clang-6.0")
+set(CMAKE_C_COMPILER "clang-11")
